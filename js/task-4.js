df4e61d1afa70bebf9b40a97523e813a0ca3afff
unknown <mais-nasinya@ukr.net>
2025-01-06 14:23:30 +0200

    second

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,7 @@ nameForm.addEventListener('submit', (evt) => {
         [nameForm.elements.email.name]: nameForm.elements.email.value.trim(),
         [nameForm.elements.password.name]: nameForm.elements.password.value.trim(),
     };
-    if (nameForm.elements.email.value === `` && nameForm.elements.password.value === ``) {
+    if (nameForm.elements.email.value === ``|| nameForm.elements.password.value === ``) {
           alert ('All form fields must be filled in')
     };
             console.log(formData)
